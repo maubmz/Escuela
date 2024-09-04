34108b0bb4bea9ba8b1ff30e1c5f842293400a3a
mau__bm <mauricio.baeza@digitalchallengers.com.mx>
2024-09-03 20:28:56 -0600

    Tarea- Uso de funciones

--- ProgramJs/program.js
+++ ProgramJs/program.js
@@ -1,18 +1,30 @@
 
-genericVariable = "Dame tu ";
+let genericVariable = "Dame tu ";
 let datos = [" edad", " peso", " estatura", " nombre", " masa corporal", " cuenta de github"];
 let arregloDatos = [];
+let lista;
+let li
 
+function pedir() {
+    if (arregloDatos.length != 0) {
+        arregloDatos = [];
+    }
+    for (let i = 0; i < 6; i++) {
+        arregloDatos.push(prompt(genericVariable + datos[i]));
+    }
+    console.log(arregloDatos)
 
-for (var i = 0; i < 6; i++) {
-    arregloDatos.push(prompt(genericVariable + datos[i]));
+    // Insertar los datos en el HTML
+    lista = document.getElementById("datos-lista");
+    arregloDatos.forEach(function(dato) {
+        li = document.createElement("li");
+        li.textContent = dato;
+        lista.appendChild(li);
+    });
+
 }
-console.log(arregloDatos)
 
-// Insertar los datos en el HTML
-let lista = document.getElementById("datos-lista");
-arregloDatos.forEach(function(dato) {
-    let li = document.createElement("li");
-    li.textContent = dato;
-    lista.appendChild(li);
-});
+function borrarDatos() {
+    lista = document.getElementById("datos-lista");
+    lista.innerHTML = ""; // Eliminará todos los elementos <li> dentro de <ul>
+}
